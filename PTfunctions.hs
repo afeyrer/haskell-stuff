7887ec0a68ce8cd3f3f0251975ed6284fdf12287
abFeigyr31 <abfeigyr31@hanovernorwichschools.org>
2019-01-28 15:39:05 +0000

    saving ptfunctions

---
+++ PTfunctions.hs
@@ -0,0 +1,13 @@
+module PTfunctions where
+quadraticVertex a b c = -b/(2*a)
+quadraticDiscriminant a b c = b*b-4*a*c
+quadraticRealSolutionQty a b c = if quadraticDiscriminant a b c > 0   
+                                    then 2 
+                                    else if quadraticDiscriminant a b c < 0 
+                                        then 0 
+                                        else 1
+convertFtoC f = (f-32)/1.8
+convertCtoF c = 1.8*c+32
+convertTemp t fc = if fc == "f"
+                    then convertFtoC t
+                    else convertCtoF t
